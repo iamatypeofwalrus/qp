5649d8fcc8889202139b79e8c3f9dd657d3d4dce
Joe Feeney <joefeeney@me.com>
2015-03-20 11:00:55 -0700

    queryParser uses Reader and Writer. Better usage message

--- main.go
+++ main.go
@@ -4,45 +4,65 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"log"
+	"io"
 	"net/url"
 	"os"
 	"strings"
 )
 
 var (
-	param   = flag.String("param", "", "A comma seperated list of params to get values for")
-	verbose = flag.Bool("verbose", false, "Print url and query string in addition to values matching params")
+	verbose    = flag.Bool("v", false, "Print url and query string in addition to values matching params")
+	nullString = flag.String("null", "NULL", "Value to print when a param is not present.")
 )
 
 func main() {
 	flag.Parse()
 
-	if *param == "" {
-		fmt.Println("You must provide a param with the -param option.")
-		return
+	if len(flag.Args()) == 0 {
+		usage()
+		os.Exit(1)
 	}
 
-	params := strings.Split(*param, ",")
+	param := flag.Args()[0]
 
-	scanner := bufio.NewScanner(os.Stdin)
+	// TODO: it may also be nice to read in from a file if it is specified
+	queryParamParser(os.Stdin, os.Stdout, getParamsArray(param))
+}
+
+func usage() {
+	fmt.Printf("Usage: qp [options] <comma delimited list of param keys>\n")
+	flag.PrintDefaults()
+
+	fmt.Print("\nExample: qp -v -null 'derp' 'format, sort'\n")
+}
+
+func getParamsArray(p string) []string {
+	p = strings.Replace(p, " ", "", -1)
+
+	return strings.Split(p, ",")
+}
+
+func queryParamParser(r io.Reader, w io.Writer, params []string) {
+	scanner := bufio.NewScanner(r)
+
+	// Allocate an array for params values once
+	vals := make([]string, len(params), len(params))
 
 	for scanner.Scan() {
 		u, err := url.Parse(scanner.Text())
 
+		// Ignore all errors due to badly formatted URLs
 		if err != nil {
-			log.Fatal(err)
+			continue
 		}
 
 		values, err := url.ParseQuery(u.RawQuery)
 
 		if err != nil {
-			log.Fatal(err)
+			continue
 		}
 
-		// Create a slice of strings that is a fixed size since we will always be
-		// looking for the same number of parameters.
-		vals := make([]string, len(params), len(params))
+		// Fill values array
 		for i, p := range params {
 			v := values.Get(p)
 
@@ -51,40 +71,52 @@ func main() {
 			}
 		}
 
+		// Check for at least one non-blank string
 		if printable(vals) {
 			if *verbose {
-				fmt.Printf("%v\t%v\t", u.Path, u.RawQuery)
+				fmt.Fprintf(w, "%v\t%v\t", u.Path, u.RawQuery)
 			}
 
-			printQueryValues(vals)
+			printQueryValues(w, vals)
+		}
+
+		// Zero out memory
+		for i := range vals {
+			vals[i] = ""
 		}
 	}
 }
 
-// printQueryValues prints the following if given a vals array of [xml, 3, derp]:
-// xml\t3\tderp\n
-func printQueryValues(vals []string) {
+// printQueryValues prints the following if given a vals array of ["xml", "3", "", "derp"]:
+// xml\t3\tNULL\tderp\n
+func printQueryValues(w io.Writer, vals []string) {
 	numVals := len(vals)
 
 	for i, val := range vals {
 
+		var printStr string
 		if val == "" {
-			fmt.Print(".")
+			printStr = *nullString
 		} else {
-			fmt.Print(val)
+			printStr = val
 		}
 
+		fmt.Fprint(w, printStr)
+
+		// Initializes to blank string ("")
 		var sep string
 		if i != numVals {
 			sep = "\t"
 		}
 
-		fmt.Print(sep)
+		fmt.Fprint(w, sep)
 	}
 
-	fmt.Println()
+	fmt.Fprintln(w)
 }
 
+// printable checks to see if there is at least one non blank string in the
+// values array
 func printable(vals []string) bool {
 	for _, v := range vals {
 		if v != "" {
